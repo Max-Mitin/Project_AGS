04569d59bf07bb11cb9d6aa480e4d62150bbf2bf
Max-Mitin <mclarensf1@mail.ru>
2025-09-25 17:46:54 +0300

    Update SQLQuery_вводы_выводы_new_from_27052024.sql

--- SQLQuery_вводы_выводы_new_from_27052024.sql
+++ SQLQuery_вводы_выводы_new_from_27052024.sql
@@ -1,63 +1,63 @@
-/* основной запрос БЕЗ пересчета в USD */
-WITH BASE AS ( select ecp.RegistrationDate AS "Дата_регистрации", ecp.Date AS "Дата_исполнения",
+/* РѕСЃРЅРѕРІРЅРѕР№ Р·Р°РїСЂРѕСЃ Р‘Р•Р— РїРµСЂРµСЃС‡РµС‚Р° РІ USD */
+WITH BASE AS ( select ecp.RegistrationDate AS "Р”Р°С‚Р°_СЂРµРіРёСЃС‚СЂР°С†РёРё", ecp.Date AS "Р”Р°С‚Р°_РёСЃРїРѕР»РЅРµРЅРёСЏ",
 ecp.SubaccOwnerFirm_ShortName AS "Client_Name", SUBSTRING(ecp.Subacc_Code, 1, 11) AS "Contract_number", 
 ecp.CurrencyAsset_ShortName AS "Currency",  
 "Inflows" = CAST(CASE 
-	WHEN (Price = 0) and CT_Const = 6 Then Size*1           /*ввод ДС*/
-	WHEN (Price > 0) and CT_Const = 4 Then Size*Price       /*ввод ЦБ*/
+	WHEN (Price = 0) and CT_Const = 6 Then Size*1           /*РІРІРѕРґ Р”РЎ*/
+	WHEN (Price > 0) and CT_Const = 4 Then Size*Price       /*РІРІРѕРґ Р¦Р‘*/
 	ELSE '0'
-	--WHEN /*(Price = 0) and*/ CT_Const = 7 Then 'no'       /*вывод ДС*/
-	--WHEN /*(Price > 0) and*/ CT_Const = 5 Then 'no'       /*вывод ЦБ*/
+	--WHEN /*(Price = 0) and*/ CT_Const = 7 Then 'no'       /*РІС‹РІРѕРґ Р”РЎ*/
+	--WHEN /*(Price > 0) and*/ CT_Const = 5 Then 'no'       /*РІС‹РІРѕРґ Р¦Р‘*/
 END AS float),
 "Outflows" = CAST(CASE
-	--WHEN /*(Price = 0) and*/ CT_Const = 6 Then 'no'          /*ввод ДС*/
-	--WHEN /*(Price > 0) and*/ CT_Const = 4 Then 'no'          /*ввод ЦБ*/
-	WHEN Price = 0 and CT_Const = 7 Then Size*1              /*вывод ДС*/
-	WHEN Price > 0 and CT_Const = 5 Then Size*Price          /*вывод ЦБ*/
+	--WHEN /*(Price = 0) and*/ CT_Const = 6 Then 'no'          /*РІРІРѕРґ Р”РЎ*/
+	--WHEN /*(Price > 0) and*/ CT_Const = 4 Then 'no'          /*РІРІРѕРґ Р¦Р‘*/
+	WHEN Price = 0 and CT_Const = 7 Then Size*1              /*РІС‹РІРѕРґ Р”РЎ*/
+	WHEN Price > 0 and CT_Const = 5 Then Size*Price          /*РІС‹РІРѕРґ Р¦Р‘*/
 	ELSE '0'
 END AS float),
 CASE ecp.CT_Const
-	WHEN 4 THEN 'ввод ЦБ'
-	WHEN 5 THEN 'вывод ЦБ'
-	WHEN 6 THEN 'ввод ДС'
-	WHEN 7 THEN 'вывод ДС'
-END AS "Тип_операции",
+	WHEN 4 THEN 'РІРІРѕРґ Р¦Р‘'
+	WHEN 5 THEN 'РІС‹РІРѕРґ Р¦Р‘'
+	WHEN 6 THEN 'РІРІРѕРґ Р”РЎ'
+	WHEN 7 THEN 'РІС‹РІРѕРґ Р”РЎ'
+END AS "РўРёРї_РѕРїРµСЂР°С†РёРё",
 ecp.Asset_ShortName,
 IsNULL(dba.ISIN, ecp.Asset_ShortName) AS "ISIN",
-ecp.Size      AS "Количество",
-ecp.Price     AS "Цена_закупки",
+ecp.Size      AS "РљРѕР»РёС‡РµСЃС‚РІРѕ",
+ecp.Price     AS "Р¦РµРЅР°_Р·Р°РєСѓРїРєРё",
 dba.id
 from backQORT_TDB.dbo.ExportCorrectPositions ecp
 left join backQORT_DB.dbo.Assets dba ON dba.ShortName = ecp.Asset_ShortName
-where ecp.Date between 20240628 and 20240704  /* выбор периода Даты исполнения. OLD RegistrationDate between 20240420 and 20240425     -- between 20240511 and 20240516  */
+where ecp.Date between 20240628 and 20240704  /* РІС‹Р±РѕСЂ РїРµСЂРёРѕРґР° Р”Р°С‚С‹ РёСЃРїРѕР»РЅРµРЅРёСЏ. OLD RegistrationDate between 20240420 and 20240425     -- between 20240511 and 20240516  */
 and CT_Const IN (4, 5, 6, 7)
-and dba.Enabled = 0             /*Удаленная запись (значение равно ID) если = 0 то запись активна */
-and ecp.IsCanceled = 'n'       /*Убрать отмененные записи, где 'y' */
+and dba.Enabled = 0             /*РЈРґР°Р»РµРЅРЅР°СЏ Р·Р°РїРёСЃСЊ (Р·РЅР°С‡РµРЅРёРµ СЂР°РІРЅРѕ ID) РµСЃР»Рё = 0 С‚Рѕ Р·Р°РїРёСЃСЊ Р°РєС‚РёРІРЅР° */
+and ecp.IsCanceled = 'n'       /*РЈР±СЂР°С‚СЊ РѕС‚РјРµРЅРµРЅРЅС‹Рµ Р·Р°РїРёСЃРё, РіРґРµ 'y' */
 ),
 
 CROSS_CURS AS (Select crh.Date, crh.OldDate, crh.TradeAsset_ID, crh.PriceAsset_ID, crh.Bid from backQORT_DB.dbo.CrossRatesHist crh 
-where OldDate = 20240516  /* Выбор даты курсов валют */
+where OldDate = 20240516  /* Р’С‹Р±РѕСЂ РґР°С‚С‹ РєСѓСЂСЃРѕРІ РІР°Р»СЋС‚ */
 and crh.TradeAsset_ID IN (9, 11, 173, 21, 112) and crh.PriceAsset_ID = 8
 ),
-						/* Дата котировок из MarketInfoHist as mih одинаковая с таблицей курсов валют CrossRatesHist as crh */
-KOTIR AS ( Select dba.ShortName, mih.LastPrice AS "Рыночная_цена", 
+						/* Р”Р°С‚Р° РєРѕС‚РёСЂРѕРІРѕРє РёР· MarketInfoHist as mih РѕРґРёРЅР°РєРѕРІР°СЏ СЃ С‚Р°Р±Р»РёС†РµР№ РєСѓСЂСЃРѕРІ РІР°Р»СЋС‚ CrossRatesHist as crh */
+KOTIR AS ( Select dba.ShortName, mih.LastPrice AS "Р С‹РЅРѕС‡РЅР°СЏ_С†РµРЅР°", 
 CASE mih.PriceAsset_ID
 	WHEN 8   THEN 'USD'
 	WHEN 9   THEN 'EUR'
 	WHEN 11  THEN 'CNY'
 	WHEN 173 THEN 'RUB'
 	WHEN 21  THEN 'GBP'
-END AS "Валюта_котировки",
+END AS "Р’Р°Р»СЋС‚Р°_РєРѕС‚РёСЂРѕРІРєРё",
 mih.PriceAsset_ID
 from backQORT_DB.dbo.MarketInfoHist mih 
 inner join backQORT_DB.dbo.Assets dba ON dba.id = mih.Asset_ID
-where mih.OldDate = 20240516  /* Выбор даты котировки для ЦБ*/
+where mih.OldDate = 20240516  /* Р’С‹Р±РѕСЂ РґР°С‚С‹ РєРѕС‚РёСЂРѕРІРєРё РґР»СЏ Р¦Р‘*/
 and dba.Enabled = 0
-and mih.TSSection_ID IN /* проблема когда из квика прилетели котировки - дубли, с ценой = 0, в итоге в отчете дубли с нулевой оценкой */
-(169, 170, 171, 173, 174, 175, 176, 177, 178, 179, 181, 184, 188, 189, 197, 198, 199, 200, 201, 186) )  /* оценка идёт с "bloomberg_%", либо с пользовательской секции */
+and mih.TSSection_ID IN /* РїСЂРѕР±Р»РµРјР° РєРѕРіРґР° РёР· РєРІРёРєР° РїСЂРёР»РµС‚РµР»Рё РєРѕС‚РёСЂРѕРІРєРё - РґСѓР±Р»Рё, СЃ С†РµРЅРѕР№ = 0, РІ РёС‚РѕРіРµ РІ РѕС‚С‡РµС‚Рµ РґСѓР±Р»Рё СЃ РЅСѓР»РµРІРѕР№ РѕС†РµРЅРєРѕР№ */
+(169, 170, 171, 173, 174, 175, 176, 177, 178, 179, 181, 184, 188, 189, 197, 198, 199, 200, 201, 186) )  /* РѕС†РµРЅРєР° РёРґС‘С‚ СЃ "bloomberg_%", Р»РёР±Рѕ СЃ РїРѕР»СЊР·РѕРІР°С‚РµР»СЊСЃРєРѕР№ СЃРµРєС†РёРё */
 
 Select
-    Дата_регистрации, Дата_исполнения,
+    Р”Р°С‚Р°_СЂРµРіРёСЃС‚СЂР°С†РёРё, Р”Р°С‚Р°_РёСЃРїРѕР»РЅРµРЅРёСЏ,
     Client_Name,
     Contract_number,
     Currency,
@@ -76,18 +76,18 @@ Select
 		WHEN Asset_ShortName = 'RUB' THEN Outflows * CROSS_CURS.Bid * (-1)
         ELSE '0'
     END AS "Outflows_USD",
-    Тип_операции,
+    РўРёРї_РѕРїРµСЂР°С†РёРё,
     Asset_ShortName,
     ISIN,
-    Количество,
-    Цена_закупки,
-    Рыночная_цена,
-    Валюта_котировки,
-    "Оценка_ЦБ_в_USD" = CASE
-        WHEN KOTIR.PriceAsset_ID = 8 THEN Рыночная_цена * Количество * 1
-        WHEN KOTIR.PriceAsset_ID = 173 THEN Рыночная_цена * Количество * Bid
-        WHEN KOTIR.PriceAsset_ID = 9 THEN Рыночная_цена * Количество * Bid
-        WHEN KOTIR.PriceAsset_ID = 21 THEN Рыночная_цена * Количество * Bid
+    РљРѕР»РёС‡РµСЃС‚РІРѕ,
+    Р¦РµРЅР°_Р·Р°РєСѓРїРєРё,
+    Р С‹РЅРѕС‡РЅР°СЏ_С†РµРЅР°,
+    Р’Р°Р»СЋС‚Р°_РєРѕС‚РёСЂРѕРІРєРё,
+    "РћС†РµРЅРєР°_Р¦Р‘_РІ_USD" = CASE
+        WHEN KOTIR.PriceAsset_ID = 8 THEN Р С‹РЅРѕС‡РЅР°СЏ_С†РµРЅР° * РљРѕР»РёС‡РµСЃС‚РІРѕ * 1
+        WHEN KOTIR.PriceAsset_ID = 173 THEN Р С‹РЅРѕС‡РЅР°СЏ_С†РµРЅР° * РљРѕР»РёС‡РµСЃС‚РІРѕ * Bid
+        WHEN KOTIR.PriceAsset_ID = 9 THEN Р С‹РЅРѕС‡РЅР°СЏ_С†РµРЅР° * РљРѕР»РёС‡РµСЃС‚РІРѕ * Bid
+        WHEN KOTIR.PriceAsset_ID = 21 THEN Р С‹РЅРѕС‡РЅР°СЏ_С†РµРЅР° * РљРѕР»РёС‡РµСЃС‚РІРѕ * Bid
         ELSE '0'
     END
 From BASE
